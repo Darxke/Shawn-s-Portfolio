4d278d5c0507012136b006c58db32a4cbb0f2299
Darxke <shixuanzhu123@gmail.com>
2024-08-12 16:57:36 -0700

    Update setup_2.py

--- src/setup_2.py
+++ src/setup_2.py
@@ -2,6 +2,11 @@
 import streamlit as st
 
 def setup():
+    col1, col2 = st.colums(2)
+    with col1:
+        st.subheader("col")
+    with col2:
+        st.subheader("col2")
     st.header(":two: Setup")
     st.write("To install Streamlit, run this command in your terminal or command prompt:")
     st.code('pip install streamlit')
