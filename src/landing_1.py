7cf11570011021eea9de64883a880263cf7eba9d
Darxke <shixuanzhu123@gmail.com>
2024-08-12 16:53:30 -0700

    Update landing_1.py

--- src/landing_1.py
+++ src/landing_1.py
@@ -11,3 +11,4 @@ st.title("Hello, Streamlit!")
 name = st.text_input("What's your name?")
 if name:
     st.write(f"Hello, {name}!")''')
+    st.info("To begin your journey, navigate through the lessons using the sidebar! 👈")
