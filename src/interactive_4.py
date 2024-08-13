7a1ccc7ecba14fb44e881b3eb1d5ee1217f79c55
Darxke <shixuanzhu123@gmail.com>
2024-08-12 17:38:09 -0700

    Update interactive_4.py

--- src/interactive_4.py
+++ src/interactive_4.py
@@ -3,8 +3,8 @@ import streamlit as st
 def interativeApp():
     st.header(":four: Simple Interative App")
     st.subheader("Let's create a basic calculator")
-    num1 = st.float_input("Please enter your first number", value = 0)
-    num2 = st.float_input("Please enter you second number", value = 0)
+    num1 = st.number_input("Please enter your first number", value = 0.1)
+    num2 = st.number_input("Please enter you second number", value = 0.1)
     operation = st.selectbox("Choose your operation", ["+", "-", "*", "/"])
     result = 0
     if st.button("Calculate"):
